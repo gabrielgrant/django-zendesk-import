c06aa74661bac03a7cffb4ce373c9d667340a9aa
Gabriel Grant <g@briel.ca>
2011-09-26 06:19:46 -0700

    fix handling of default user and break up 'handle' method

--- zendesk_import/management/commands/askbot_import_from_zendesk.py
+++ zendesk_import/management/commands/askbot_import_from_zendesk.py
@@ -15,28 +15,40 @@ class Command(BaseCommand):
         default_userid_setting_name = 'ASKBOT_IMPORT_FROM_ZENDESK_DEFAULT_USER_ID'
         default_userid = getattr(settings, default_userid_setting_name, None)
         if default_userid is None:
-            default_user = None
+            self.default_user = None
         else:
-            default_user = askbot_models.User.objects.get(id=default_userid)
+            self.default_user = askbot_models.User.objects.get(id=default_userid)
         # import entries as questions using forums as tags
-        entries = zd_models.Entry.objects  \
-            .filter(is_public=True)  \
-            .exclude(submitter__django_user=None)
+        entries_public = zd_models.Entry.objects.filter(is_public=True)
+        if self.default_user is None:
+            entries = entries_public.exclude(submitter__django_user=None)
+        else:
+            entries = entries_public
 
-        print 'Importing %s Zendesk Entries as Askbot Questions' % len(entries)
+        print 'Importing %s of %s public Zendesk Entries as Askbot Questions' % (
+            len(entries), len(entries_public)
+        )
         for entry in entries:
-            q = entry.submitter.django_user.post_question(
-                title=entry.title,
-                body_text=entry.body,
-                tags=re.sub(r'\s+', '_', entry.forum.name.lower()),
-                timestamp=entry.updated_at,
+            self.import_entry_as_question(entry)
+    def import_entry_as_question(self, entry):
+        u = entry.submitter.django_user or self.default_user
+        q = u.post_question(
+            title=entry.title,
+            body_text=entry.body,
+            tags=re.sub(r'\s+', '_', entry.forum.name.lower()),
+            timestamp=entry.updated_at,
+        )
+        # import posts as answers
+        answer_count = 0
+        for post in entry.posts.all():
+            self.import_post_as_answer(post, q)
+            answer_count += 1
+        print 'Imported %s Posts as Answers to "%s"' % (answer_count, q)
+    def import_post_as_answer(self, post, question):
+        u = post.user.django_user or self.default_user
+        if u is not None:
+            u.post_answer(
+                question=question,
+                body_text=post.body,
+                timestamp=post.updated_at,
             )
-            # import posts as answers
-            for post in entry.posts.all():
-                u = post.user.django_user or default_user
-                if u is not None:
-                    u.post_answer(
-                        question=q,
-                        body_text=post.body,
-                        timestamp=post.updated_at,
-                    )
